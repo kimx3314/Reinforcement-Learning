610b3b8693c825c84974697173e113bfb92f5466
Sean Sungil Kim <35079022+kimx3314@users.noreply.github.com>
2020-03-20 15:25:25 +0900

    Update dqn_config.py

--- DQN/dqn_config.py
+++ DQN/dqn_config.py
@@ -5,8 +5,9 @@ class Config(object):
         # parameters
         self.BATCH_SIZE = 256
         self.EPISODES = 2000
+        self.TARGET_UPDATE_STEP = 100         # update the target model every N steps
         self.LR = 0.001                       # learning rate for the NN optimizer
-        self.GAMMA = 0.98                     # discount rate for the bellman equation
+        self.GAMMA = 0.99                     # discount rate for the bellman equation
         self.EPSILON = 1.0                    # exploration rate, starting value set at 100% chance of exploring
         self.MIN_EPSILON = 0.001              # minimum exploration rate
         self.EPSILON_DECAY = 0.999            # the rate at which the exploration rate decays
