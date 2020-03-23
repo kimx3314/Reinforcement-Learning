bf898f985a4b059d345a709e9850db62a6f206a1
Sean Sungil Kim <35079022+kimx3314@users.noreply.github.com>
2020-03-23 18:02:02 +0900

    Update dqn_tf_config.py

--- DQN/dqn_tf_config.py
+++ DQN/dqn_tf_config.py
@@ -6,8 +6,9 @@ class Config(object):
         self.BATCH_SIZE = 128
         self.EPISODES = 2000
         self.TARGET_UPDATE_STEP = 100         # update the target model every N steps
-        self.LR = 0.001                       # learning rate for the NN optimizer
+        self.LR = 0.002                       # learning rate for the NN optimizer
         self.GAMMA = 0.99                     # discount rate for the bellman equation
+        self.TAU = 0.01                       # for soft update
         self.EPSILON = 1.0                    # exploration rate, starting value set at 100% chance of exploring
         self.MIN_EPSILON = 0.001              # minimum exploration rate
         self.EPSILON_DECAY = 0.999            # the rate at which the exploration rate decays
