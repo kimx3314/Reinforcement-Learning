1eeca932d7c9e6c9d8f11f924e6261a62db27131
Sean Sungil Kim <35079022+kimx3314@users.noreply.github.com>
2020-03-23 17:21:27 +0900

    Add files via upload

---
+++ DQN/dqn_keras_main.py
@@ -0,0 +1,58 @@
+# sean sungil kim
+
+import gym
+from dqn_keras_networks import DQNAgent
+from dqn_keras_config import Config
+
+
+# set the config class
+config = Config()
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v0')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    step = 1
+
+    dqn = DQNAgent(state_size, action_size, config)
+
+    for episode in range(config.EPISODES):
+        # for each episode, reset the environment
+        state = env.reset()
+
+        score = 0
+        while True:
+            if config.RENDER:
+                env.render()
+            
+            # t
+            # retrieve the action from the ddpg model
+            action = dqn.act(state)
+
+            # input the action to the environment, and obtain the following
+            next_state, reward, done, _ = env.step(action)
+            step += 1
+            
+            # if the episode is finished, reward is -10
+            reward = -10 if done else reward
+
+            # store in the replay experience queue and go to the next state
+            dqn.remember(state, action, reward, next_state, done)
+            
+            # t + 1
+            # go to the next state
+            state = next_state
+            score += reward
+
+            # if the episode is finished, go to the next episode
+            if done:
+                print("Episode: %i / %i,\tScore: %i,\tExploration Rate: %.4f" % (episode + 1, config.EPISODES, score, config.EPSILON))
+                break
+
+            # if there are enough instances in the replay experience queue, fit the NN using past experience
+            if len(dqn.memory) > config.BATCH_SIZE:
+                dqn.train()
+
+            # update the target_model every N steps
+            if step % config.TARGET_UPDATE_STEP == 0:
+                dqn.update_target_model()
---
+++ DQN/dqn_keras_networks.py
@@ -0,0 +1,99 @@
+# sean sungil kim
+
+import numpy as np
+from tensorflow import one_hot
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+import keras.backend as K
+from collections import deque
+import random
+
+
+class DQNAgent:
+    def __init__(self, state_size, action_size, config):
+        # initialize the parameters and the model
+        self.config = config
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen = self.config.MEMORY_CAPACITY)
+
+        self.target_model = self.build_model()
+        self.model = self.build_model()
+
+    def build_model(self):
+        # building the NN model
+        model = Sequential()
+
+        # input shape should be the state size
+        model.add(Dense(32, input_dim = self.state_size, activation = 'relu'))
+        model.add(Dense(32, activation = 'relu'))
+
+        # output shape should be the action size
+        model.add(Dense(self.action_size, activation = 'linear'))
+        model.compile(loss = self.custom_loss, optimizer = Adam(lr = self.config.LR))
+
+        return model
+
+    def custom_loss(self, yTrue, yPred):
+        return K.mean(K.square(yTrue - np.amax(yPred)))
+
+    def act(self, state):
+        #reshape according to the input shape
+        state = state.reshape(-1, self.state_size)
+
+        # if the exploration rate is below or equal to the random sample from a uniform distribution over [0, 1), return a random action
+        if np.random.rand() <= self.config.EPSILON:
+            return random.randrange(self.action_size)
+        
+        # return the action with the highest rewards, exploitation
+        else:
+            return np.argmax(self.model.predict(state))
+
+    def remember(self, state, action, reward, next_state, done):
+        # store in the replay experience queue
+        self.memory.append((state, action, reward, next_state, done))
+
+    def bellman(self, batch_data, q_values):
+        # return the bellman total return
+        q_values = np.array(batch_data['reward']) - np.array(1 - np.array(batch_data['done']) * self.config.GAMMA * np.amax(self.target_model.predict(np.array(batch_data['next_state'])), axis = 1))
+
+        return q_values
+
+    def train(self):
+        field_names = ['state', 'action', 'reward', 'next_state', 'done']
+        batch_data = {}
+
+        # randomly sample from the replay experience que
+        replay_batch = random.sample(self.memory, self.config.BATCH_SIZE)
+        for i in range(len(field_names)):
+            batch_data[field_names[i]] = [data for data in list(zip(*replay_batch))[i]]
+        batch_data.update({'done' : [int(bl) for bl in batch_data['done']]})
+
+        # calculate the q-values in the current state and update the total return
+        q_values = self.model.predict(np.array(batch_data['state']))
+        q_values = self.bellman(batch_data, q_values)
+
+        # fit the model using the replay experience with updated q_values
+        self.model.fit(np.array(batch_data['state']), q_values, epochs = 10, verbose = 0)
+        #history = self.model.fit(state, q_values, epochs = 1, verbose = 0)
+        #loss = history.history['loss'][0]
+        #self.model_loss.append(loss)
+
+        # if the exploration rate is greater than the set minimum, apply the decay rate
+        if self.config.EPSILON > self.config.MIN_EPSILON:
+            self.config.EPSILON *= self.config.EPSILON_DECAY
+
+        #return loss
+
+    def update_target_model(self):
+        # update weights of the target_model (with weights of the model)
+        self.target_model.set_weights(self.model.get_weights())
+
+    def load(self, name):
+        # load saved model
+        self.model.load_weights(name)
+
+    def save(self, name):
+        # save model weights
+        self.model.save_weights(name)
